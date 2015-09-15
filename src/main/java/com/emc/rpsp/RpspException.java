a761d39f503e9f90b14ff488ab098139515d2f62
Boris Shpilyuck <boris.shpilyuck@emc.com>
2015-09-15 22:54:15 +0300

    Git UI change 3.2

--- src/main/java/com/emc/rpsp/RpspException.java
+++ src/main/java/com/emc/rpsp/RpspException.java
@@ -3,6 +3,7 @@ package com.emc.rpsp;
 /**
  * Created by morand3 on 1/14/2015.
  */
+//change 3.3
 public class RpspException extends RuntimeException {
 	public RpspException(String msg) {
 		super(msg);
